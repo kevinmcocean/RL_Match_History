f098485c882d9c9573696bcc86cc20f985244e78
mcoceanflow <mcoceanflow@gmail.com>
2025-11-15 00:39:11 -0800

    Added Session class.

---
+++ Session.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "GuiBase.h"
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/pluginwindow.h"
+#include "bakkesmod/plugin/PluginSettingsWindow.h"
+
+class Session
+{
+private:
+
+	// Variables
+	int currentGameType = -1;
+
+};
